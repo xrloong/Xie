95cba496ceec6fe6abc5de643d5868f15b3528ed
Xanadu Wang <xanaduw@synology.com>
2016-06-15 19:53:52 +0800

    Add save()/restore() to save and restore the drawing state for DrawingSystem.

--- xie/graphics/drawing.py
+++ xie/graphics/drawing.py
@@ -2,6 +2,8 @@ class DrawingSystem():
 	def __init__(self, canvasController):
 		self.canvasController=canvasController
 		self.lastPoint = None
+		self.sourceBoundary = None
+		self.sourceBoundaryState = []
 
 	def getWidth(self):
 		return self.canvasController.getWidth()
@@ -19,26 +21,50 @@ class DrawingSystem():
 	def _clearLastPoint(self):
 		self.lastPoint = None
 
-	def startDrawing(self, startPoin):
-		self._moveTo(startPoin)
-
-	def endDrawing(self):
-		self._clearLastPoint()
-
 	def _convertPoint(self, point):
 		startPoint=self.lastPoint
 		return (startPoint[0]+point[0], startPoint[1]+point[1])
 
+	def _convertPointByBoundary(self, point):
+		if self.sourceBoundary:
+			tx, ty = 0, 0
+			tW, tH = self.getWidth(), self.getHeight()
+			sx, sy = self.sourceBoundary.getLeft(), self.sourceBoundary.getTop()
+			sW, sH = self.sourceBoundary.getWidth(), self.sourceBoundary.getHeight()
+			x, y = point
+
+			return (tx + (x-sx)*tW/sW, ty + (y-sy)*tH/sH)
+		else:
+			return point
+
+	def setSourceBoundary(self, sourceBoundary):
+		self.sourceBoundary = sourceBoundary
+
+	def save(self):
+		self.sourceBoundaryState.append(self.sourceBoundary)
+
+	def restore(self):
+		self.sourceBoundary = self.sourceBoundaryState.pop()
+
+	def startDrawing(self, startPoin):
+		self._moveTo(self._convertPointByBoundary(startPoin))
+
+	def endDrawing(self):
+		self._clearLastPoint()
+
 	def lineTo(self, point):
 		canvasController=self.canvasController
-		p=self._convertPoint(point)
+		p=self._convertPoint(self._convertPointByBoundary(point))
 		canvasController.lineTo(p)
 		self.lastPoint=p
 
 	def qCurveTo(self, p1, p2):
 		canvasController=self.canvasController
-		p1=self._convertPoint(p1)
-		p2=self._convertPoint(p2)
+		p1=self._convertPoint(self._convertPointByBoundary(p1))
+		p2=self._convertPoint(self._convertPointByBoundary(p2))
 		canvasController.qCurveTo(p1, p2)
 		self.lastPoint=p2
+
+	def clear(self):
+		self.canvasController.clear()
 
--- xie/graphics/shape.py
+++ xie/graphics/shape.py
@@ -1,11 +1,63 @@
 # coding=utf8
 
 class Shape:
+	pass
+
+class Boundary:
+	def __init__(self, left, top, right, bottom):
+		assert left <= right and top <= bottom
+		self.left=left
+		self.top=top
+		self.right=right
+		self.bottom=bottom
+
+	def getLeft(self):
+		return self.left
+
+	def getTop(self):
+		return self.top
+
+	def getRight(self):
+		return self.right
+
+	def getBottom(self):
+		return self.bottom
+
+	def getTopLeft(self):
+		return (self.left, self.top)
+
+	def getTopRight(self):
+		return (self.right, self.top)
+
+	def getBottomLeft(self):
+		return (self.left, self.bottom)
+
+	def getBottomRight(self):
+		return (self.right, self.bottom)
+
+	def getWidth(self):
+		return self.right - self.left
+
+	def getHeight(self):
+		return self.bottom - self.top
+
+Boundary.Default = Boundary(0, 0, 256, 256)
+#	def getBoundary(self):
+#		return (self.left, self.top, self.right, self.bottom)
+
+class Drawable:
+	def __init__(self, boundary = Boundary.Default):
+		self.boundary = boundary
+
+	def getBoundary(self):
+		return self.boundary
+
 	def draw(self, drawSystem):
 		pass
 
-class Rectangle(Shape):
+class Rectangle(Drawable):
 	def __init__(self, x=0, y=0, w=0, h=0):
+		super().__init__()
 		self.x=x
 		self.y=y
 		self.w=w
