94c29160c0e31d76e63f03936698443259aac6df
xrloong <xrloong@gmail.com>
2017-03-05 03:45:12 +0800

    Add test cases

--- xie.py
+++ xie.py
@@ -25,14 +25,25 @@ class XieApp:
 		from xie.graphics.shape import Rectangle
 
 		frame=Rectangle(0, 0, canvasController.getWidth(), canvasController.getHeight())
-		frame.draw(self.dh)
+		self.dh.draw(frame)
 
 		targetRect = Rectangle(0, 0, 512, 512)
 
 	def mainloop(self):
 		self.root.mainloop()
 
+	def test(self):
+		from xie.graphics.stroke import BeelineSegment
+		from xie.graphics.stroke import StrokePath
+		from xie.graphics.stroke import Stroke
+
+		segment=BeelineSegment((200, 0))
+		strokePath=StrokePath([segment])
+		stroke=Stroke((70, 30), strokePath)
+		self.dh.draw(stroke)
+
 if __name__ == '__main__':
 	app=XieApp()
+	app.test()
 	app.mainloop()
 
