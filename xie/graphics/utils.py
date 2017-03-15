27e79e49af76a0f981f54c1ac2b88ad409bacb95
xrloong <xrloong@gmail.com>
2017-03-15 23:24:08 +0800

    Change the text format for drawing a stroke. To use 5 digits but not 8 digits to present a point.

--- xie/graphics/utils.py
+++ xie/graphics/utils.py
@@ -3,13 +3,13 @@ class TextCodec:
 		pass
 
 	def encodeStartPoint(self, p):
-		return "0000{0[0]:02X}{0[1]:02X}".format(p)
+		return "0{0[0]:02X}{0[1]:02X}".format(p)
 
 	def encodeEndPoint(self, p):
-		return "0001{0[0]:02X}{0[1]:02X}".format(p)
+		return "1{0[0]:02X}{0[1]:02X}".format(p)
 
 	def encodeControlPoint(self, p):
-		return "0002{0[0]:02X}{0[1]:02X}".format(p)
+		return "2{0[0]:02X}{0[1]:02X}".format(p)
 
 	def encodeStrokeExpression(self, pointExpressionList):
 		return ",".join(pointExpressionList)
@@ -18,17 +18,17 @@ class TextCodec:
 		return ";".join(strokeExpressionList)
 
 	def isStartPoint(self, pointExpression):
-		return pointExpression[3]=='0'
+		return pointExpression[0]=='0'
 
 	def isEndPoint(self, pointExpression):
-		return pointExpression[3]=='1'
+		return pointExpression[0]=='1'
 
 	def isControlPoint(self, pointExpression):
-		return pointExpression[3]=='2'
+		return pointExpression[0]=='2'
 
 	def decodePointExpression(self, pointExpression):
 		e=pointExpression
-		return (int(e[4:6], 16), int(e[6:8], 16))
+		return (int(e[1:3], 16), int(e[3:5], 16))
 
 	def decodeStrokeExpression(self, strokeExpression):
 		return strokeExpression.split(",")
