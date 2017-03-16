427c654fb3afd0a9fffc8bfced577f2416edd082
Xanadu Wang <xanaduw@synology.com>
2017-03-16 18:41:59 +0800

    Change to new format to present stroke drawing.

--- xie/graphics/utils.py
+++ xie/graphics/utils.py
@@ -1,21 +1,29 @@
 class TextCodec:
+	STROKE_SEPERATOR = "/"
+	POINT_SEPERATOR = ","
+	PARAMETER_SEPERATOR = "."
+
+	START_POINT_PATTERN="0" + PARAMETER_SEPERATOR + "{0[0]}" + PARAMETER_SEPERATOR + "{0[1]}"
+	END_POINT_PATTERN="1" + PARAMETER_SEPERATOR + "{0[0]}" + PARAMETER_SEPERATOR + "{0[1]}"
+	CONTROL_POINT_PATTERN="2" + PARAMETER_SEPERATOR + "{0[0]}" + PARAMETER_SEPERATOR + "{0[1]}"
+
 	def __init__(self):
 		pass
 
 	def encodeStartPoint(self, p):
-		return "0{0[0]:02X}{0[1]:02X}".format(p)
+		return TextCodec.START_POINT_PATTERN.format(p)
 
 	def encodeEndPoint(self, p):
-		return "1{0[0]:02X}{0[1]:02X}".format(p)
+		return TextCodec.END_POINT_PATTERN.format(p)
 
 	def encodeControlPoint(self, p):
-		return "2{0[0]:02X}{0[1]:02X}".format(p)
+		return TextCodec.CONTROL_POINT_PATTERN.format(p)
 
 	def encodeStrokeExpression(self, pointExpressionList):
-		return ",".join(pointExpressionList)
+		return TextCodec.POINT_SEPERATOR.join(pointExpressionList)
 
 	def encodeCharacterExpression(self, strokeExpressionList):
-		return ";".join(strokeExpressionList)
+		return TextCodec.STROKE_SEPERATOR.join(strokeExpressionList)
 
 	def isStartPoint(self, pointExpression):
 		return pointExpression[0]=='0'
@@ -27,12 +35,12 @@ class TextCodec:
 		return pointExpression[0]=='2'
 
 	def decodePointExpression(self, pointExpression):
-		e=pointExpression
-		return (int(e[1:3], 16), int(e[3:5], 16))
+		param=pointExpression.split(TextCodec.PARAMETER_SEPERATOR)
+		return (int(param[1]), int(param[2]))
 
 	def decodeStrokeExpression(self, strokeExpression):
-		return strokeExpression.split(",")
+		return strokeExpression.split(TextCodec.POINT_SEPERATOR)
 
 	def decodeCharacterExpression(self, characterExpression):
-		return characterExpression.split(";")
+		return characterExpression.split(TextCodec.STROKE_SEPERATOR)
 
