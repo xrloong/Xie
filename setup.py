56779adbb5cabbf688bbafe730b785a3fff38dcf
xrloong <xrloong@gmail.com>
2017-03-18 00:42:23 +0800

    Change version to 0.0.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.0.7',
+	version='0.0.8',
 
 	description='試圖以筆劃描述漢字的函式庫',
 #	long_description=long_description,
