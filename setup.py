9b7c76e41c42361e235009536d03de0c6d5834ad
Xanadu Wang <xanaduw@synology.com>
2021-12-07 23:08:14 +0800

    Update the config and script for setup

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version=find_version("xie", "__init__.py"),
 
 	description='試圖以筆劃描述漢字的函式庫',
 #	long_description=long_description,
